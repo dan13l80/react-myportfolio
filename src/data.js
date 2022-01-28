cc19f9d5b1f7cb6cd00febaacfd3c211a33e6a77
dan13l80 <88808310+dan13l80@users.noreply.github.com>
2022-01-28 03:48:17 -0500

    data structure supporting content

---
+++ src/data.js
@@ -0,0 +1,61 @@
+export const projects = [
+    {
+      title: "Budget Tracker",
+      subtitle: "Add expenses and deposits to their budget with or without a connection",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.png",
+      link: "https://github.com/dan13l80/Budget-Tracker",
+    },
+    {
+      title: "Tech Blog",
+      subtitle: "Blog fot techinical thoughts",
+      description:
+        "A tech blog that allows users to sign up, login, and logout. Users can write posts about their tech thoughts, and comment on the tech thoughts of other users.",
+      image: "./project-2.png",
+      link: "https://github.com/dan13l80/Tech-Blog",
+    },
+    {
+      title: "Weather Dashboard",
+      subtitle: "The weather outlook for multiple cities",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.png",
+      link: "https://github.com/dan13l80/Weather-Dashboard",
+    },
+    {
+      title: "Password Generator",
+      subtitle: "Randomly generate a password",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.png",
+      link: "https://github.com/dan13l80/Password-Generator",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      image: "https://randomuser.me/api/portraits/women/93.jpg",
+      name: "Deborah Daniel",
+      company: "Catapiller",
+    },
+    {
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      image: "https://randomuser.me/api/portraits/men/94.jpg",
+      name: "Evan Jotham",
+      company: "Blinkcoze Inc",
+    },
+  ];
+  
+  export const skills = [
+    "JavaScript",
+    "React",
+    "HTML",
+    "CSS",
+    "Powerpoint",
+    "Photoshop",
+  ];
+  
