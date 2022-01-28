bcfd56d1c5dec93be6b4ef0bcfcc90362a474a60
dan13l80 <88808310+dan13l80@users.noreply.github.com>
2022-01-28 03:45:03 -0500

    about component updated

---
+++ src/components/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-Grey">
+            Hello, I am <b>Daniel Subramanian.</b>
+            <br className="hidden lg:inline-block" />Fullstack Web Developer. 
+          </h1>
+          <p className="mb-8 leading-relaxed">
+          I Love building apps and website. I love to learn about new technologies and simply play games in my free time.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              Contact Me
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./dp.png"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
